41efe95b3cd5011bfbc324f9d6062e787110ef0e
Cifer0101 <l.kakabadze2020@gmail.com>
2024-04-11 02:10:12 +0400

    FAQ Page with MUI Accordion

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,5 +1,86 @@
+import { Box, useTheme } from "@mui/material"
+import Header from "../../components/Header"
+import Accordion from "@mui/material/Accordion"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import Typography from "@mui/material/Typography"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import { tokens } from "../../theme"
+
 const FAQ = () => {
-  return <div>FAQ</div>
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant={"h5"} color={colors.greenAccent[500]}>
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quaerat soluta
+            modi non porro, quo magnam laboriosam autem veritatis accusantium!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant={"h5"} color={colors.greenAccent[500]}>
+            Another Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quaerat soluta
+            modi non porro, quo magnam laboriosam autem veritatis accusantium!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant={"h5"} color={colors.greenAccent[500]}>
+            Your Favorite Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quaerat soluta
+            modi non porro, quo magnam laboriosam autem veritatis accusantium!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant={"h5"} color={colors.greenAccent[500]}>
+            Some Random Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quaerat soluta
+            modi non porro, quo magnam laboriosam autem veritatis accusantium!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant={"h5"} color={colors.greenAccent[500]}>
+            The Final Qustion
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quaerat soluta
+            modi non porro, quo magnam laboriosam autem veritatis accusantium!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  )
 }
 
 export default FAQ
