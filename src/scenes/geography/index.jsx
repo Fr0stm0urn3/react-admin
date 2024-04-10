f342945154fbde737a59bd1022127508c17a8ae9
Cifer0101 <l.kakabadze2020@gmail.com>
2024-04-11 03:50:50 +0400

    GeoGraphyChart Component + Geography page

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/material"
+import Header from "../../components/Header"
+import GeographyChart from "../../components/GeographyChart"
+
 const Geography = () => {
-  return <div>Geography</div>
+  return (
+    <Box m="20px">
+      <Header title="Geography" subtitle="Simple Geography Chart" />
+      <Box height="75vh">
+        <GeographyChart />
+      </Box>
+    </Box>
+  )
 }
 
 export default Geography
