246e89627f2caa13b4f9d09faf32bf73c4bfc602
Cifer0101 <l.kakabadze2020@gmail.com>
2024-04-11 02:57:40 +0400

    BarChart Component Bar page

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,5 +1,16 @@
+import BarChart from "../../components/BarChart"
+import Header from "../../components/Header"
+import { Box } from "@mui/material"
+
 const Bar = () => {
-  return <div>Bar</div>
+  return (
+    <Box m="20px">
+      <Header title="Bar" subtitle={"Simple Bar Chart"} />
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </Box>
+  )
 }
 
 export default Bar
